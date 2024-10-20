8d4479dd83c368d9a41992cfe0c10fe11acd2388
Mary-Anne Eze <ifyeze@gmail.com>
2024-10-20 20:07:29 +0100

    Update User data
    Updated

--- inc/user_data.php
+++ inc/user_data.php
@@ -3,7 +3,7 @@
 if (session_status() == PHP_SESSION_ACTIVE) {
 
     $fullname =  $_SESSION['full_name'];
-    echo $username =  $_SESSION['username'];
+    $username =  $_SESSION['username'];
     $email_address =  $_SESSION['email_address'];
 } else {
     // echo '<script>window.location.href="login.php"</script>';
